47913a10f85b72bd9365968b328947d3f558a00c
Christian Bracher <cbracher69@gmail.com>
2014-08-09 15:10:03 -0700

    Added new options to graphics engine
    Includes new example plots

--- PPMI Graphics Library/PPMI_Gaussplots.py
+++ PPMI Graphics Library/PPMI_Gaussplots.py
@@ -1,5 +1,9 @@
 # Fancy Gaussian display styles for 1D and 2D maps.
 
+# Update (August 9, 2014):
+# - Add choice of cumulative or probability density distribution
+# - Add white background scatterplot method
+
 # Provide a colored scatterplot that uses Gaussian profiles 
 # to show distribution besides markers.
 # Indicate averages for different cohorts if applicable
@@ -37,16 +41,16 @@ def scatter_gauss(x_series, y_series, subj_cond):
 
     # Displayed Canvas
 
-    x_min = -2.0
-    x_max = +2.0
+    x_min = -2.5
+    x_max = +2.5
 
-    y_min = -2.0
-    y_max = +2.0
+    y_min = -2.5
+    y_max = +2.5
     
     # Resolution of background image:
 
-    image_resolution_x = 0.02
-    image_resolution_y = 0.02
+    image_resolution_x = 0.025
+    image_resolution_y = 0.025
     
     # Number of data points to be displayed
     
@@ -105,12 +109,12 @@ def scatter_gauss(x_series, y_series, subj_cond):
     ImageRGB[:,:,2] = ImageB
     
     # Set up figure parameters:
-    fig = plt.figure()
+    fig = plt.figure(num=None, figsize=(8, 8), dpi=150, facecolor='w', edgecolor='k')
 
     plt.xlim(x_min, x_max)
     plt.ylim(y_min, y_max)
 
-    plt.title(x_series.name + ' vs. ' + y_series.name)
+    plt.title(x_series.name + ' - ' + y_series.name)
 
     # Create figure background - Gaussian distributions
     
@@ -172,14 +176,14 @@ def scatter_gauss(x_series, y_series, subj_cond):
         ax.scatter(x_select, y_select, s = 10, c = 'r', alpha = .33)
         ax.scatter(x_avg, y_avg, s = 150 , c = 'r', marker = '*', label = 'SWEDD')    
 
-    ax.legend(loc = 'upper left')
+    ax.legend(loc = 'upper left', scatterpoints = 1)
     fig.add_subplot(ax)
 
     # Now, save the graph as a .PNG image.
     # Don't write it to disk - instead, return it as a string:
     
     imgdata = StringIO.StringIO()
-    fig.savefig(imgdata, format='png')
+    fig.savefig(imgdata, dpi = 150, format='png')
     plt.close()
     
     # Get the contents of the image file as a string:
@@ -189,6 +193,109 @@ def scatter_gauss(x_series, y_series, subj_cond):
     
     return png_file
 
+# The same as the Gaussian scatter plot, just without the fancy background:
+
+def scatter_plain(x_series, y_series, subj_cond):
+    
+    # This section ideally should be controlled by function parameter keys.
+    # For now, these were pretty good values for the PPMI study: 
+
+    # Displayed Canvas
+
+    x_min = -2.5
+    x_max = +2.5
+
+    y_min = -2.5
+    y_max = +2.5
+    
+    # Number of data points to be displayed
+    
+    scatter_size = len(x_series)
+    
+    # Color information - dictionary of rgb values for each color code (saturated)
+    # Here only defined for types 0, 1, 2 (HC, PD, SWEDD)
+    # (One could extend this for more values & colors, of course.)
+
+    rgb = {'HC':[0.0,0.0,1.0], 'PD':[0.0,1.0,0.0], 'SWEDD':[1.0,0.0,0.0]}
+       
+    # Set up figure parameters:
+    fig = plt.figure(num=None, figsize=(8, 8), dpi=150, facecolor='w', edgecolor='k')
+
+    plt.xlim(x_min, x_max)
+    plt.ylim(y_min, y_max)
+
+    plt.title(x_series.name + ' - ' + y_series.name)
+
+    # Create pinpoints for individual measurements by condition:
+    # Healthy controls:
+
+    ax = plt.subplot()
+    
+    cond_filter = (subj_cond == 'HC')
+    
+    x_select = x_series[cond_filter]
+    y_select = y_series[cond_filter]
+    
+    if (len(x_select) > 0):
+        
+        x_avg = x_select.mean()
+        y_avg = y_select.mean()
+        
+        # Plot little blue markers for data points, big stars for cohort average
+
+        ax.scatter(x_select, y_select, s = 20, c = 'b', alpha = .33)
+        ax.scatter(x_avg, y_avg, s = 175 , c = 'b', marker = '*', label = 'HC')
+    
+    # Same now for Parkinson's cohort:
+
+    cond_filter = (subj_cond == 'PD')
+    
+    x_select = x_series[(cond_filter)]
+    y_select = y_series[(cond_filter)]
+    
+    if (len(x_select) > 0):
+        
+        x_avg = x_select.mean()
+        y_avg = y_select.mean()
+
+        # Plot little green markers for data points, big stars for cohort average
+       
+        ax.scatter(x_select.tolist(), y_select.tolist(), s = 20, c = 'g', alpha = .33)
+        ax.scatter(x_avg, y_avg, s = 175, c = 'g', marker = '*', label = 'PD')
+     
+    # And finally, the SWEDD cohort:
+
+    cond_filter = (subj_cond == 'SWEDD')
+    
+    x_select = x_series[(cond_filter)]
+    y_select = y_series[(cond_filter)]
+    
+    if (len(x_select) > 0):
+        
+        x_avg = x_select.mean()
+        y_avg = y_select.mean()
+
+        # Plot little red markers for data points, big stars for cohort average
+        
+        ax.scatter(x_select, y_select, s = 20, c = 'r', alpha = .33)
+        ax.scatter(x_avg, y_avg, s = 175, c = 'r', marker = '*', label = 'SWEDD')    
+
+    ax.legend(loc = 'upper left', scatterpoints = 1)
+    fig.add_subplot(ax)
+
+    # Now, save the graph as a .PNG image.
+    # Don't write it to disk - instead, return it as a string:
+    
+    imgdata = StringIO.StringIO()
+    fig.savefig(imgdata, dpi = 150, format='png')
+    plt.close()
+    
+    # Get the contents of the image file as a string:
+    
+    imgdata.seek(0)
+    png_file = imgdata.buf
+    
+    return png_file
 
 # A Gaussian display style for 1D profiles of PPMI data.
 
@@ -203,13 +310,14 @@ def scatter_gauss(x_series, y_series, subj_cond):
 # Corresponding series object (subject condition/cohort) - subj_cond
 # A list of all subject cohorts present - cohorts
 # A pandas Series object containing the number of subjects in each cohort - data_counts
+# A switch between CDF and PDF (by default, CDF is shown)
 #
 # Returns:
 # String containing bitmap image in .PNG format - png_file
 #
 # Note: This depends on the normal distribution provided by scipy.stats.norm
 
-def profile_gauss(x_series, subj_cond, cohorts, data_counts):
+def profile_gauss(x_series, subj_cond, cohorts, data_counts, cumulative = True):
     
     # This section ideally should be controlled by function parameter keys.
     # For now, these were pretty good values for the PPMI study: 
@@ -220,8 +328,8 @@ def profile_gauss(x_series, subj_cond, cohorts, data_counts):
     
     # Displayed Canvas
 
-    x_min = -2.0
-    x_max = +2.0
+    x_min = -2.5
+    x_max = +2.5
 
     y_min = 0.0
     y_max = 1.0
@@ -243,37 +351,50 @@ def profile_gauss(x_series, subj_cond, cohorts, data_counts):
     
     # Create a discrete function value table for each cohort present in the test set
     
-    cdf_dict = {}
+    df_dict = {}
     
     for c in cohorts:
-        cdf_dict[c] = np.zeros(gridpoints)
+        df_dict[c] = np.zeros(gridpoints)
         
     # Sum up weighted normal cdf centered around each datapoint:
         
     for subj in subj_cond.index:
                 
-        # Cumulative distribution function:
+        # Choosen cumulative distribution function (CDF) or probability density (PDF):
         
-        normal_cdf = norm.cdf(x, loc = x_series[subj], scale = Gauss_width)
+        if (cumulative == True):
+            normal_df = norm.cdf(x, loc = x_series[subj], scale = Gauss_width)
+        else:
+            normal_df = norm.pdf(x, loc = x_series[subj], scale = Gauss_width)
         
-        # Add it to proper cohort function:
+        # Add it to proper cohort function (and normalize it):
         
-        cdf_dict[subj_cond[subj]] += normal_cdf / data_counts[subj_cond[subj]]
+        df_dict[subj_cond[subj]] += normal_df / data_counts[subj_cond[subj]]
            
     # Subplot for resulting cdf's for cohorts present:
     
-    fig = plt.figure()
-    
-    plt.xlim(x_min, x_max)
-    plt.ylim(y_min, y_max)
+    fig = plt.figure(num=None, figsize=(8, 8), dpi=150, facecolor='w', edgecolor='k')
     
     ax = plt.subplot2grid((4,1), (0,0), rowspan=3)
     plt.title(x_series.name)
     
+    # Function value limits depend on method chosen:
+
+    if (cumulative == True):
+        y_max = 1.0
+    else:
+        # Find maximum function value in set of cohorts
+
+        y_max = 0
+        for c in cohorts:
+            y_max = max(y_max, df_dict[c].max())
+
+    plt.axis([x_min, x_max, 0, 1.01 * y_max])
+    
     for member in cohorts:
-        ax.plot(x, cdf_dict[member], lw = 2, c = rgb[member], label = member)
+        ax.plot(x, df_dict[member], lw = 2, c = rgb[member], label = member)
         
-    ax.legend(loc = 'upper left')
+    ax.legend(loc = 'upper left', scatterpoints = 1)
     fig.add_subplot(ax)
     
     # Create subplot with pinpoints for individual measurements by condition:
@@ -339,7 +460,7 @@ def profile_gauss(x_series, subj_cond, cohorts, data_counts):
     # Don't write it to disk - instead, return it as a string:
     
     imgdata = StringIO.StringIO()
-    fig.savefig(imgdata, format='png')
+    fig.savefig(imgdata, dpi = 150, format='png')
     plt.close()
     
     # Get the contents of the image file as a string:
